6dfaf670178c18dbc1af5fece57f9f27c8399b7a
David Rubin <david.rubin95@gmail.com>
2016-04-08 18:42:29 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -100,6 +100,13 @@ $(document).ready(function() {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
   });
+  
+  socket.on('dregljaj', function(dregljaj) {
+    $('#vsebina').jrumble();
+    $('#vsebina').trigger('startRumble');
+    setTimeout(function(){ $('#vsebina').trigger('stopRumble'); }, 1500);
+    
+  });
 
   setInterval(function() {
     socket.emit('kanali');
